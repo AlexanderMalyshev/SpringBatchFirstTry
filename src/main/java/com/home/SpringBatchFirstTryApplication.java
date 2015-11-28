b8676fff53bfb23fc6e4ab79ed474817efd5bc44
Alexander <malishvmk@mail.ru>
2015-11-28 10:39:50 +0300

    working copy from spring example

--- src/main/java/com/home/SpringBatchFirstTryApplication.java
+++ src/main/java/com/home/SpringBatchFirstTryApplication.java
@@ -3,9 +3,11 @@ package com.home;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableBatchProcessing
+//@ImportResource("classpath:/context/batch.xml")
 public class SpringBatchFirstTryApplication {
 
     public static void main(String[] args) {
