7e58c9fef62a3b4db759d849379db88051b501ed
Alexander <malishvmk@mail.ru>
2015-12-01 23:42:09 +0300

    2 options: via xml or java

--- src/main/java/com/home/configuration/BatchConfiguration.java
+++ src/main/java/com/home/configuration/BatchConfiguration.java
@@ -19,6 +19,7 @@ import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -26,69 +27,70 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableBatchProcessing
+@ImportResource("classpath:batch.xml")
 public class BatchConfiguration {
-    @Autowired
-    DataSource dataSource;
+//    @Autowired
+//    DataSource dataSource;
 
     // tag::readerwriterprocessor[]
-    @Bean
-    public ItemReader<Person> reader() {
-        FlatFileItemReader<Person> reader = new FlatFileItemReader<Person>();
-        reader.setResource(new ClassPathResource("sample-data.csv"));
-        reader.setLineMapper(new DefaultLineMapper<Person>() {{
-            setLineTokenizer(new DelimitedLineTokenizer() {{
-                setNames(new String[] { "firstName", "lastName" });
-            }});
-            setFieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
-                setTargetType(Person.class);
-            }});
-        }});
-        return reader;
-    }
+//    @Bean
+//    public ItemReader<Person> reader() {
+//        FlatFileItemReader<Person> reader = new FlatFileItemReader<Person>();
+//        reader.setResource(new ClassPathResource("sample-data.csv"));
+//        reader.setLineMapper(new DefaultLineMapper<Person>() {{
+//            setLineTokenizer(new DelimitedLineTokenizer() {{
+//                setNames(new String[] { "firstName", "lastName" });
+//            }});
+//            setFieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
+//                setTargetType(Person.class);
+//            }});
+//        }});
+//        return reader;
+//    }
 
-    @Bean
-    public ItemProcessor<Person, Person> processor() {
-        return new PersonItemProcessor();
-    }
+//    @Bean
+//    public ItemProcessor<Person, Person> processor() {
+//        return new PersonItemProcessor();
+//    }
 
-    @Bean
-    public ItemWriter<Person> writer() {
-        JdbcBatchItemWriter<Person> writer = new JdbcBatchItemWriter<Person>();
-        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Person>());
-        writer.setSql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)");
-        writer.setDataSource(dataSource);
-        return writer;
-    }
+//    @Bean
+//    public ItemWriter<Person> writer() {
+//        JdbcBatchItemWriter<Person> writer = new JdbcBatchItemWriter<Person>();
+//        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Person>());
+//        writer.setSql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)");
+//        writer.setDataSource(dataSource);
+//        return writer;
+//    }
     // end::readerwriterprocessor[]
 
     // tag::jobstep[]
-    @Bean
-    public Job importUserJob(JobBuilderFactory jobs, Step s1, JobExecutionListener listener) {
-        return jobs.get("importUserJob")
-                .incrementer(new RunIdIncrementer())
-                .listener(listener)
-                .flow(s1)
-                .end()
-                .build();
-    }
-
-    @Bean
-    public Step step1(StepBuilderFactory stepBuilderFactory, ItemReader<Person> reader,
-                      ItemWriter<Person> writer, ItemProcessor<Person, Person> processor,
-                      ChunkListener chunkListener, StepExecutionListener stepExecutionListener) {
-        return stepBuilderFactory.get("step1")
-                .<Person, Person> chunk(3)
-                .reader(reader)
-                .processor(processor)
-                .writer(writer)
-                .listener(chunkListener)
-                .listener(stepExecutionListener)
-                .build();
-    }
+//    @Bean
+//    public Job importUserJob(JobBuilderFactory jobs, Step s1, JobExecutionListener listener) {
+//        return jobs.get("importUserJob")
+//                .incrementer(new RunIdIncrementer())
+//                .listener(listener)
+//                .flow(s1)
+//                .end()
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step1(StepBuilderFactory stepBuilderFactory, ItemReader<Person> reader,
+//                      ItemWriter<Person> writer, ItemProcessor<Person, Person> processor,
+//                      ChunkListener chunkListener, StepExecutionListener stepExecutionListener) {
+//        return stepBuilderFactory.get("step1")
+//                .<Person, Person> chunk(3)
+//                .reader(reader)
+//                .processor(processor)
+//                .writer(writer)
+//                .listener(chunkListener)
+//                .listener(stepExecutionListener)
+//                .build();
+//    }
     // end::jobstep[]
 
-    @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource);
-    }
+//    @Bean
+//    public JdbcTemplate jdbcTemplate() {
+//        return new JdbcTemplate(dataSource);
+//    }
 }
