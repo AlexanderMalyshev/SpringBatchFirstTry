02e869b244afb8c35bab4171edd1e07603098607
Alexander <malishvmk@mail.ru>
2015-12-01 23:08:00 +0300

    + Java 8 streams

--- src/main/java/com/home/listeners/JobNotificationListener.java
+++ src/main/java/com/home/listeners/JobNotificationListener.java
@@ -1,49 +1,24 @@
 package com.home.listeners;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
-import com.home.data.Person;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import com.home.data.PersonDao;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class JobNotificationListener extends JobExecutionListenerSupport {
-
-	private static final Logger log = LoggerFactory.getLogger(JobNotificationListener.class);
-
-	private final JdbcTemplate jdbcTemplate;
-
-	@Autowired
-	public JobNotificationListener(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
+    @Autowired
+    PersonDao personDao;
 
 	@Override
 	public void afterJob(JobExecution jobExecution) {
 		if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
 			log.info("-> JOB FINISHED! Time to verify the results");
-
-			List<Person> results = jdbcTemplate.query("SELECT first_name, last_name FROM people", new RowMapper<Person>() {
-				@Override
-				public Person mapRow(ResultSet rs, int row) throws SQLException {
-					return new Person(rs.getString(1), rs.getString(2));
-				}
-			});
-
-			for (Person person : results) {
-				log.info("Found <" + person + "> in the database.");
-			}
-
+            personDao.findAll().forEach(person -> log.info("Found <" + person + "> in the database."));
 		}
 	}
 
